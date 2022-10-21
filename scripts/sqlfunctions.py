b00a1f90af057dd5a47d7ef874421e410496070f
Manu Vleurick <manu.vleurick@student.hogent.be>
2022-10-21 12:03:01 +0200

    nieuwe functie get_score_for_word toegevoegd nog niet getest

--- scripts/sqlfunctions.py
+++ scripts/sqlfunctions.py
@@ -157,3 +157,28 @@ def getRawDuurzaamheidsrapport(ondernemingsnummer,pg_conn=None):
     if connaanwezig==False:
         pg_conn.close()
     return result.first()
+#returnt 0 als het niet voorkomt, 1 als het op website voorkomt, 2 als het op duurzaamheidsrapport voorkomt en 3 als het op beide voorkomt
+def get_score_for_word(ondernemingsnummer,word,pg_conn=None):
+    #connect met de databank
+    if pg_conn is None:
+        pg_conn = get_database()
+        connaanwezig=False
+    else:
+        connaanwezig=True
+    #search functie
+    result_website=pg_conn.execute('select ondernemingsnummer from raw_data where "ondernemingsNummer"= %s and "ts_website" @@ to_tsquery("dutch",%s);', (ondernemingsnummer,word)).first()
+    result_duurzaamheidsrapport=pg_conn.execute('select ondernemingsnummer from raw_data where "ondernemingsNummer"= %s and "ts_duurzaamheidsrapport" @@ to_tsquery("dutch",%s);', (ondernemingsnummer,word)).first()
+    
+    score = 0
+    if len(result_website) == 1:
+        if len(result_duurzaamheidsrapport) == 1:
+            score =  3
+        else:
+            score = 1
+    else:
+        if len(result_duurzaamheidsrapport) == 1:
+            score = 2
+
+    if connaanwezig==False:
+        pg_conn.close()
+    return score
