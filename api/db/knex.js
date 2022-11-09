ceeb43f9dbf0905ecc4746db9346b7d0163d28ec
Ewout De Coster <ewout.decoster@student.hogent.be>
2022-11-09 16:54:21 +0100

    changed pg to postgres

--- api/db/knex.js
+++ api/db/knex.js
@@ -2,7 +2,7 @@
 /**Maakt de connectie met de databank door middel van knex */
 const knex = require("knex");
 connectedKnex = knex({
-  client: "pg",
+  client: "postgres",
   connection: {
     host: "vichogent.be",
     port: 40035,
