6e7dbfce4dceb7532ce25902d2dc54b6ceb5290c
Jarno Reynaert <jarno.reynaert@student.hogent.be>
2022-12-10 11:24:55 +0100

    ml changes

--- scripts/sqlfunctions.py
+++ scripts/sqlfunctions.py
@@ -258,15 +258,17 @@ def getRawDuurzaamheidsrapport(ondernemingsnummer, pg_conn=None):
 
 def get_datapunten_voor_model(pg_conn=None):
     pg_conn = get_database()
-    # select functie
-    result = pg_conn.execute('select rd."ondernemingsNummer", f.turnover as omzet, f.beursgenoteerd, k."sector_ID" as sector, e.total as total_employees, a.capital_city, null, null, s.score \
-                             from raw_data rd \
-                             join adress a on a."ondernemingsNummer"=rd."ondernemingsNummer" \
-                             join finance f on rd."ondernemingsNummer"=f."ondernemingsNummer" \
-                             join employees e on f."ondernemingsNummer"=e."ondernemingsNummer" \
-                             join kmo_sector k on k."ondernemingsNummer"=e."ondernemingsNummer" \
-                             join score s on s."forain_ID"=e."ondernemingsNummer" \
-                             ').fetchall()
+    result = pg_conn.execute("""
+        select rd."ondernemingsNummer",f.turnover as omzet, f.beursgenoteerd, k."sector_ID" as sector, e.total as total_employees, \
+        a.capital_city, CASE WHEN rd.website = '' or rd.website is null THEN 0 ELSE 1 END AS site_aanwezig, \
+        case when rd.jaarrekening = '' or rd.jaarrekening is null then 0 else 1 end as pdf_aanwezig, s.score as total_score \
+        from raw_data rd  \
+        join adress a on a."ondernemingsNummer"=rd."ondernemingsNummer" \
+        join finance f on rd."ondernemingsNummer"=f."ondernemingsNummer" \
+        join employees e on f."ondernemingsNummer"=e."ondernemingsNummer" \
+        join kmo_sector k on k."ondernemingsNummer"=e."ondernemingsNummer" \
+        join score s on s."forain_ID"=e."ondernemingsNummer";
+                             """).fetchall()
     pg_conn.close()
     return result
 # returnt 0 als het niet voorkomt, 1 als het op website voorkomt, 2 als het op duurzaamheidsrapport voorkomt en 3 als het op beide voorkomt
