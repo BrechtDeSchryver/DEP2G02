58da9151f5d57d06cbcf9d5cd9449d32ddbb9c78
Brecht De Schryver <brecht.deschryver@student.hogent.be>
2022-12-13 23:45:25 +0100

    test predict

--- scripts/Flaskapi.py
+++ scripts/Flaskapi.py
@@ -10,6 +10,19 @@ running="done"
 def test():
     global running
     time.sleep(10)
+@app.route("/api/predict/Omzet=<Omzet>&personeel=<personeel>&postcode=<postcode>&sector=<sector>&jr=<jaarrekening>&beurs=<beursgenoteerd>",methods=['GET'])
+def predcit(Omzet,personeel,postcode,sector,jaarrekening,beursgenoteerd):
+    #Omzet= omzetcijfer (int)
+    #personeel= aantal personeelsleden (int)
+    #postcode= postcode (4 char int)
+    #sector= sectornacebel (5 char int)
+    #jaarrekening= jaarrekening aanwezig (boolean)
+    #beursgenoteerd= beursgenoteerd (boolean)
+    score=0
+    #hier moet de predict komen en uitkomst stop je in score
+    print(Omzet,personeel,postcode,sector,jaarrekening,beursgenoteerd)
+    x = {"test" : score}
+    return json.dumps(x)
 @app.route("/api/recalculate/<code>",methods=['GET'])
 def get_posts(code):
     global running
@@ -36,4 +49,4 @@ def uptime():
   x = {"status" : "online"}
   return json.dumps(x)
 
-app.run(host='0.0.0.0', debug=True, port=8080)
+app.run(host='127.0.0.1', debug=True, port=6969)
