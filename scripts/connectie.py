af29c92e36a8859c5c5f20f0093092ea28819900
Jarno Reynaert <jarno.reynaert@student.hogent.be>
2022-12-19 19:32:04 +0100

    perongeluk connectie aangepast

--- scripts/connectie.py
+++ scripts/connectie.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 # get credentials from api\db\credentials.env file
-load_dotenv(r"C:\Users\jarno\Desktop\hogent\jaar 3\sem 1\Data project 2\DEP2G02\api\db\credentials.env")
+load_dotenv("api/db/credentials.env")
 password = os.getenv('DBPASSWD')
 db_user = os.getenv('DBUSER')
 host = os.getenv('DBHOST')
 port = os.getenv('DBPORT')
 def get_database():
-    # print(f'postgresql://{str(db_user)}:{str(password)}@{host}:{port}/dep')
     return create_engine(f'postgresql://{str(db_user)}:{str(password)}@{host}:{port}/dep').connect()
 # base = declarative_base()
 # pg_conn = pg_engine.connect()
@@ -17,4 +16,3 @@ def get_database():
 # Session = sessionmaker(bind=pg_engine)
 # pg_session = Session()
 # base.metadata.create_all(pg_engine)
-
